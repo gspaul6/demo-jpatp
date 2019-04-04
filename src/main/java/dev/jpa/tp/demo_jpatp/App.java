de3cdf18d676f708645e6751c8d6e12ca61f7064
Paul <gspaul6@gmail.com>
2019-04-04 12:27:07 +0200

    first one

---
+++ src/main/java/dev/jpa/tp/demo_jpatp/App.java
@@ -0,0 +1,13 @@
+package dev.jpa.tp.demo_jpatp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
