c6c8aa3f306af1464a50f488f5d6374b12165d11
Paul <gspaul6@gmail.com>
2019-04-04 12:53:10 +0200

    second

--- src/main/java/dev/jpa/tp/demo_jpatp/App.java
+++ src/main/java/dev/jpa/tp/demo_jpatp/App.java
@@ -1,5 +1,12 @@
 package dev.jpa.tp.demo_jpatp;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
 /**
  * Hello world!
  *
@@ -8,6 +15,26 @@ public class App
 {
     public static void main( String[] args )
     {
+    	// Etape 1 - Créer une instance d'EntityManagerFactory
+    			EntityManagerFactory emf = Persistence.createEntityManagerFactory("demo-jpatp");
+    			
+    			// Début d'une unité de travail
+    			EntityManager em1 = emf.createEntityManager();
+    			
+    			// création d'une requête
+    			TypedQuery<Livre> requete = em1.createQuery("select l from Livre l", Livre.class);
+    			
+    			List<Livre> listeLivres = requete.getResultList();
+    			
+    			listeLivres.forEach(unLivre -> {
+    				System.out.println(unLivre.getId() + " - "  + unLivre.getTitre() + " - " + unLivre.getAuteur());
+    			});
+    			
+    			
+    			// Fin d'une unité de travail
+    			em1.close();
+    			
+    			emf.close();
         System.out.println( "Hello World!" );
     }
 }
---
+++ src/main/java/dev/jpa/tp/demo_jpatp/Livre.java
@@ -0,0 +1,47 @@
+package dev.jpa.tp.demo_jpatp;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="LIVRE")
+public class Livre {
+	@Id // obligatoire
+	@Column(name="ID")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(name="TITRE")
+	private String titre;
+	
+	@Column(name="AUTEUR")
+	private String auteur;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitre() {
+		return titre;
+	}
+
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+
+	public String getAuteur() {
+		return auteur;
+	}
+
+	public void setAuteur(String auteur) {
+		this.auteur = auteur;
+	}
+	
+}
